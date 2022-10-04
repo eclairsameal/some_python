3c1e2e458821f5840246729974e8b00bf856b936
Fenrir <fenrireclair@gmail.com>
2022-10-04 21:11:36 +0900

    Create ch12_19_1.py

---
+++ L12_Object-oriented programming/ch12_19_1.py
@@ -0,0 +1,14 @@
+class A():
+    def __init__(self):
+        print("class A")
+        
+class B():
+    def __init__(self):
+        print("class B")
+
+class C(A, B):
+    def __init__(self):
+        super().__init__()
+        print("class C")
+    
+x = C()
